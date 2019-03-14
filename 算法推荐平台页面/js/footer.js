970a9aabfbc73e5b9fba1704b2d82e84ef594eaf
WUJINH <37758615+WUJINH@users.noreply.github.com>
2019-03-14 12:08:36 +0800

    Add files via upload

---
+++ 算法推荐平台页面/js/footer.js
@@ -0,0 +1,7 @@
+let time = document.querySelector(".time");
+	time.innerHTML = new Date();
+let run = function() {
+	let str = nTime = new Date();
+		time.innerHTML = str;
+}
+setInterval(run,1000);
